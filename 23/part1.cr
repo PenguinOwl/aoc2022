c32925fcc448ca54bb535c6ce87127a5cbeee5dd
Edith Boles <tirededith@gmail.com>
2022-12-23 01:31:02 -0800

    day 23

---
+++ 23/part1.cr
@@ -0,0 +1,117 @@
+#! /usr/bin/crystal
+require "../shared.cr"
+
+input = File.read("input.txt").strip
+
+alias Vector = Tuple(Int32, Int32)
+
+class Elf
+  property proposed : Vector? = nil
+end
+
+grid = {} of Vector => Elf
+elves = [] of Elf
+
+input.lines.each_with_index do |line, y|
+  line.chars.each_with_index do |char, x|
+    next unless char == '#'
+    elf = Elf.new
+    grid[{x, y}] = elf
+    elves << elf
+  end
+end
+
+directions = [{0, -1}, {0, 1}, {-1, 0}, {1, 0}]
+
+10.times do
+  # puts directions
+  grid.keys.each do |pos|
+    elf = grid[pos]
+    can_move = false
+    (-1..1).each do |x|
+      (-1..1).each do |y|
+        unless x.zero? && y.zero?
+          if grid[{pos[0] + x, pos[1] + y}]?
+            can_move = true
+          end
+        end
+      end
+    end
+    next unless can_move
+    directions.each do |direction|
+      failed = false
+      if direction[0] == 0
+        (-1..1).each do |x_offset|
+          if grid[{x_offset + pos[0], pos[1] + direction[1]}]?
+            failed = true
+          end
+        end
+        next if failed
+        elf.proposed = {pos[0], pos[1] + direction[1]}
+        break
+      elsif direction[1] == 0
+        (-1..1).each do |y_offset|
+          if grid[{pos[0] + direction[0], y_offset + pos[1]}]?
+            failed = true
+          end
+        end
+        next if failed
+        elf.proposed = {pos[0] + direction[0], pos[1]}
+        break
+      end
+    end
+  end
+  possible = elves.select{|e| e.proposed}
+  locations = possible.map(&.proposed)
+  movers = possible.reject{|e| locations.count(e.proposed) > 1}
+  # puts possible, movers
+  movers.each do |elf|
+    pos = grid.key_for(elf)
+    grid.delete(pos)
+    if oof = grid[elf.proposed.not_nil!]?
+      # pp grid
+      # puts elf, elf.proposed
+      # puts oof, oof.proposed
+      exit 1
+    end
+    grid[elf.proposed.not_nil!] = elf
+  end
+  elves.each{|e| e.proposed = nil}
+  directions.rotate!
+  # render(grid)
+end
+
+def render(grid)
+  min_x = grid.keys.map(&.[0]).min
+  max_x = grid.keys.map(&.[0]).max
+  min_y = grid.keys.map(&.[1]).min
+  max_y = grid.keys.map(&.[1]).max
+
+  min_y.upto(max_y) do |y|
+    min_x.upto(max_x) do |x|
+      if elf = grid[{x, y}]?
+        print '#'
+      else
+        print '.'
+      end
+    end
+    print '\n'
+  end
+end
+
+min_x = grid.keys.map(&.[0]).min
+max_x = grid.keys.map(&.[0]).max
+min_y = grid.keys.map(&.[1]).min
+max_y = grid.keys.map(&.[1]).max
+
+count = 0
+
+min_y.upto(max_y) do |y|
+  min_x.upto(max_x) do |x|
+    unless grid[{x, y}]?
+      count += 1
+    end
+  end
+end
+
+puts count
